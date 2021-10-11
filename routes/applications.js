674453c53cc680f695f4cb9b6ecff3156177bb73
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2021-10-11 15:25:36 +0530

    push on heroku

---
+++ routes/applications.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+let application = require('../models/application');
+
+router.route('/').post((req, res) => {
+  const newApplication= new application({post:req.body.post,name:req.body.name,expectedSalary:parseInt(req.body.expectedSalary),noticePeriod:parseInt(req.body.noticePeriod),qualification:req.body.qualification});
+  newApplication.save()
+    .then(() => res.json('application added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+
+
+router.route('/post').get((req, res) => {
+  application.find({post:req.body.post})
+    .then(j => res.json(j))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
