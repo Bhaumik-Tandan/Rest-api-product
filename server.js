05b074a41adf5067202adf71f7ad09fb7c224041
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2021-10-16 21:08:29 +0530

    deployed

--- server.js
+++ server.js
@@ -9,9 +9,11 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true }
 );
+
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
